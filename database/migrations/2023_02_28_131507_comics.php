6fface98ad142d28fe72dc402676c4b0f5d8023c
Ali-Chouchene <choucheneali9801@gmail.com>
2023-02-28 16:00:22 +0100

    db fixed

---
+++ database/migrations/2023_02_28_131507_comics.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb', 505);
+            $table->decimal('price', 8, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string("type");
+            $table->text('artists');
+            $table->text('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            $str_artists = implode(',', $comic['artists']);
+            $new_comic->artists = $str_artists;
+            $str_writers = implode(',', $comic['writers']);
+            $new_comic->writers = $str_writers;
+            // dd($new_comic);
+
+            $new_comic->save();
+        }
+    }
+}
